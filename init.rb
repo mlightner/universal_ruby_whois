552a05680ff85064d805d31080d622404a556449
mlightner <mlightner@gmail.com>
2008-12-05 20:38:28 -0700

    Added missing slash in filename.

--- init.rb
+++ init.rb
@@ -1,3 +1,3 @@
 # All we need to do to initialize this as a Rails plugin is require the library.
 
-require File.dirname(__FILE__) + 'lib/universal_ruby_whois'
+require File.dirname(__FILE__) + '/lib/universal_ruby_whois'
